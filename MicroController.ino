40d6ee041e811e94c6e84d311d4f6b651a43d5c8
jichinthaka <imesh.14@cse.mrt.ac.lk>
2017-07-14 01:10:40 +0530

    sd

--- MicroController.ino
+++ MicroController.ino
@@ -11,71 +11,77 @@ SoftwareSerial BT(4, 2);
 
 IRsend irsend;
 
-void dump(decode_results *results) {
-  // Dumps out the decode_results structure.
-  // Call this after IRrecv::decode()
+String prepareString(decode_results *results) {
+ 
+  String output=String();
+  String intermediate=String();
   int count = results->rawlen;
   if (results->decode_type == UNKNOWN) {
-    Serial.print("Unknown encoding: ");
+   output=String("Unknown:");
   }
   else if (results->decode_type == NEC) {
-    Serial.print("Decoded NEC: ");
+     output=String("NEC: ");
 
   }
   else if (results->decode_type == SONY) {
-    Serial.print("Decoded SONY: ");
+     output=String("SONY: ");
   }
   else if (results->decode_type == RC5) {
-    Serial.print("Decoded RC5: ");
+     output=String("RC5: ");
   }
   else if (results->decode_type == RC6) {
-    Serial.print("Decoded RC6: ");
+     output=String("RC6: ");
   }
   else if (results->decode_type == PANASONIC) {
-    Serial.print("Decoded PANASONIC - Address: ");
-    Serial.print(results->address, HEX);
-    Serial.print(" Value: ");
+    output=String("PANASONIC");
+    String p1=String(results->address, HEX);
+    output=String(output+p1);
   }
   else if (results->decode_type == LG) {
-    Serial.print("Decoded LG: ");
+     output=String("Decoded LG: ");
   }
   else if (results->decode_type == JVC) {
-    Serial.print("Decoded JVC: ");
+     output=String("Decoded JVC: ");
   }
   else if (results->decode_type == AIWA_RC_T501) {
-    Serial.print("Decoded AIWA RC T501: ");
+     output=String("Decoded AIWA RC T501: ");
   }
   else if (results->decode_type == WHYNTER) {
-    Serial.print("Decoded Whynter: ");
+     output=String("Decoded Whynter: ");
   }
-  Serial.print(results->value, HEX);
-  Serial.print(" (");
-  Serial.print(results->bits, DEC);
-  Serial.println(" bits)");
-  Serial.print("Raw (");
-  Serial.print(count, DEC);
-  Serial.print("): ");
 
+  String p2=String(results->address,HEX);
+  output=String(output+p2+":");
+  
+  String i1=String(results->value,HEX);
+  output=String(output+i1+":");
+  
+  String i2=String(results->bits, DEC);
+  output=String(output+i2+":");
+  
   for (int i = 1; i < count; i++) {
     if (i & 1) {
-      Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
+      String i4=String(results->rawbuf[i]*USECPERTICK, DEC);
+      output=String(output+i4+"_");
     }
     else {
-      Serial.write('-');
-      Serial.print((unsigned long) results->rawbuf[i]*USECPERTICK, DEC);
+      output=String(output+'-');
+      String i5=String((unsigned long) results->rawbuf[i]*USECPERTICK, DEC);
+      output=String(output+i5+"_");
     }
-    Serial.print(" ");
+    output=String(output+": ");
   }
-  Serial.println();
+
+  String i3=String(count, DEC);
+  output=String(output+i3+":");
+  
+  Serial.println(output);
+  return output;
 }
 
 void setup() {
-  // initialize serial communications
   Serial.begin(9600); 
   BT.begin(9600);
-
-  // In case the interrupt driver crashes on setup, give a clue
-  // to the user what's going on.
   Serial.println("Enabling IRin");
   irrecv.enableIRIn(); // Start the receiver
   Serial.println("Enabled IRin");
@@ -84,27 +90,60 @@ void setup() {
 
 void loop() {
 
-  ////////////////
+  if (BT.available())
+  {
+     char a=(BT.read());
+     Serial.println(a);
+     if(a=='+'){
+      sendToPhone();
+     }
+     else if(a=='*'){
+      receiveFromPhone();
+     } 
+   }  
+/*
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
-    dump(&results);
+    Serial.println("************************");
+    String outputResult=prepareString(&results);
+    delay(100);
+    Serial.println("************************");
     irrecv.resume(); // Receive the next value
     BT.println("aaa");
-    BT.println(results.value, HEX);
+//    BT.println(
+//      results.decode_type+':'+
+//      results.address+':'+
+//      results.value+':'+
+//      results.bits+':'+
+//      results.&rawbuf+':'+
+//      results.rawlen+':'+
+//      results.overflow
+//    );
     //BT.write(results.value);
   }
   delay(100);
-
-  /////////////
-
   if (BT.available())
-  // if text arrived in from BT serial...
   {
    char a=(BT.read());
    Serial.println(a);
-    
-  }
-
-  
-
+  }  
+  */
 }
+
+void sendToPhone(){
+    if (irrecv.decode(&results)) {
+      String outputResult=prepareString(&results);
+      delay(100);
+      irrecv.resume(); // Receive the next value
+      BT.println(outputResult);
+    }
+}
+
+void receiveFromPhone(){
+  String input=String("");
+  while (BT.available())
+  {
+   input=String(input+(BT.read()));
+  }
+  Serial.println(input);
+}
+
